f9d449fb7ecd3e3253bc7f0546d207c021684979
TheoLvs <theo.alves.da.costa@gmail.com>
2017-08-08 18:30:49 +0200

    first commit grid world

---
+++ ai/rl/grid_world.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- 
+
+
+"""--------------------------------------------------------------------
+REINFORCEMENT LEARNING
+Grid World
+
+Started on the 08/08/2017
+
+
+References : 
+- https://www.youtube.com/watch?v=A5eihauRQvo&t=5s
+- https://github.com/llSourcell/q_learning_demo
+
+
+theo.alves.da.costa@gmail.com
+https://github.com/theolvs
+------------------------------------------------------------------------
+"""
+
+
+import os
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np 
+import sys
+import random
+import time
+
+
+
